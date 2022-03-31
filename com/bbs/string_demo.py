3f0bd2b690cdb05d6acc811239a07838a6c83d93
cc <cqu20141693@gmail.com>
2022-03-31 23:05:54 +0800

    添加字符串api

--- com/bbs/string_demo.py
+++ com/bbs/string_demo.py
@@ -26,3 +26,38 @@ print('------------------------------')
 
 print('hello\nrunoob')  # 使用反斜杠(\)+n转义特殊字符
 print(r'hello\nrunoob')  # 在字符串前面添加一个 r，表示原始字符串，不会发生转义
+print("""who are you,
+I am cc.
+""")
+'''
+字符串api:
+split: 分割字符串 bacada.split(a)=[b,c,d]
+join: 链接字符串，a.join([b,c,d])=bacada
+
+'''
+
+
+# def 定义方法
+def reverseWords(inputs):
+    # 通过空格将字符串分隔符，把各个单词分隔为列表
+    inputWords = inputs.split(" ")
+    print("字符串分割api:split-->", inputWords)
+    # 翻转字符串
+    # 假设列表 list = [1,2,3,4],
+    # list[0]=1, list[1]=2 ，而 -1 表示最后一个元素 list[-1]=4 ( 与 list[3]=4 一样)
+    # inputWords[-1::-1] 有三个参数
+    # 第一个参数 -1 表示最后一个元素
+    # 第二个参数为空，表示移动到列表末尾
+    # 第三个参数为步长，-1 表示逆向
+    inputWords = inputWords[-1::-1]
+
+    # 重新组合字符串
+    output = ' '.join(inputWords)
+
+    return output
+
+
+if __name__ == "__main__":
+    inputs = 'I like runoob'
+    rw = reverseWords(inputs)
+    print(rw)
