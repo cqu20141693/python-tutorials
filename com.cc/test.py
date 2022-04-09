13e0010ffcef896383eb70cdef1417a9e98690cc
xinzai <1361667570@qq.com>
2022-04-09 21:17:51 +0800

    序列

--- com.cc/test.py
+++ com.cc/test.py
@@ -3,7 +3,6 @@ import random
 
 names = input("请输入11个人得姓名；")
 names1 = names.rstrip("'").lstrip("'").split(',')  # 主要是切割字符串
-print(names1)
 a = len(names1)
 b = int(a * 0.3)
 print(random.sample(names1, b))
